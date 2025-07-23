76eaf113e8e1d0496d71953789b424efa5e0a6fc
Alex Perez <alexperez@mulesoft.com>
2025-07-23 17:25:17 -0300

    Enhance web-test-runner configuration to support Playwright for multiple browsers, add coverage settings, and adjust test timeouts. Update GitHub Actions workflow to run tests using the new configuration.

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,12 +1,30 @@
+import { playwrightLauncher } from '@web/test-runner-playwright';
+
 export default {
-	files: 'test/**/*.test.js',
-	nodeResolve: true,
-	middleware: [
-		function rewriteBase(context, next) {
-			if (context.url.indexOf('/base') === 0) {
-				context.url = context.url.replace('/base', '')
-			}
-			return next();
-		}
-	]
+  files: 'test/**/*.test.js',
+  nodeResolve: true,
+  coverage: true,
+  browsers: [
+    playwrightLauncher({ product: 'chromium' }),
+    playwrightLauncher({ product: 'firefox' }),
+    playwrightLauncher({ product: 'webkit' })
+  ],
+  testFramework: {
+    config: {
+      timeout: '60000',
+    },
+  },
+  browserStartTimeout: 60000,
+  testsStartTimeout: 60000,
+  testsFinishTimeout: 60000,
+  coverageConfig: {
+    include: ['src/**/*.js'],
+    exclude: ['test/**/*.js', 'demo/**/*.js'],
+    threshold: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80
+    }
+  }
 };

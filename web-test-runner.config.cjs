131b2abcb094077d58105cb5381c000d50578d5c
Alex Perez <alexperez@mulesoft.com>
2025-07-24 01:13:27 -0300

    Refactor web-test-runner configuration to use ES module syntax, enhance timeout settings, and integrate visual regression plugin. Update GitHub Actions workflow to use specific versions for actions and streamline dependency installation, improving CI performance and compatibility.

--- web-test-runner.config.cjs
+++ web-test-runner.config.cjs
@@ -1,50 +1,40 @@
-const { playwrightLauncher } = require('@web/test-runner-playwright');
+import { visualRegressionPlugin } from '@web/test-runner-visual-regression/plugin'
 
-module.exports = {
+export default {
   files: 'test/**/*.test.js',
   nodeResolve: true,
-  coverage: true,
-  browsers: [
-    playwrightLauncher({ product: 'chromium' }),
-    playwrightLauncher({ product: 'firefox' }),
-    playwrightLauncher({ product: 'webkit' })
-  ],
-  testFramework: {
-    config: {
-      timeout: '60000',
-    },
-  },
-  browserStartTimeout: 60000,
-  testsStartTimeout: 60000,
-  testsFinishTimeout: 60000,
-  coverageConfig: {
-    include: ['src/**/*.js'],
-    exclude: ['test/**/*.js', 'demo/**/*.js'],
-    threshold: {
-      statements: 80,
-      branches: 80,
-      functions: 80,
-      lines: 80
-    }
-  },
   middleware: [
     function rewriteBase(context, next) {
-      if (context.url.startsWith('/base/')) {
-        context.url = context.url.replace('/base/', '/');
+      if (context.url.indexOf('/base') === 0) {
+        context.url = context.url.replace('/base', '')
       }
-      return next();
-    }
+      return next()
+    },
   ],
-  testRunnerHtml: testFramework => `
-    <html>
-      <head>
-        <script>
-          window.process = { env: { NODE_ENV: "development" } };
-        </script>
-      </head>
-      <body>
-        <script type="module" src="${testFramework}"></script>
-      </body>
-    </html>
-  `
-}; 
+  coverageConfig: {
+    include: ['src/**.js'],
+  },
+  testFramework: {
+    config: {
+      timeout: 800000,
+    },
+  },
+  browserStartTimeout: 20000,
+  testsStartTimeout: 20000,
+  testsFinishTimeout: 800000,
+  plugins: [
+    visualRegressionPlugin({
+      update: process.argv.includes('--update-visual-baseline'),
+      diffOptions: {
+        threshold: 1,
+      },
+    }),
+  ],
+  testRunnerHtml: (testFramework) =>
+    `<html>
+  <body>
+    <script src="./demo/vendor.js"></script>
+    <script type="module" src="${testFramework}"></script>
+  </body>
+  </html>`,
+}
